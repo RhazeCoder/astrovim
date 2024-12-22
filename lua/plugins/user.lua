6f31ad35dc7695592e42a1c6738dd37811d1e469
Justine Agcanas <agcanasjustine1@gmail.com>
2024-12-22 20:39:39 +0800

    feat: change banner

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -22,7 +22,7 @@ return {
     "goolord/alpha-nvim",
     opts = function(_, opts)
       -- customize the dashboard header
-      opts.section.header.val = {{
+      opts.section.header.val = {
         "    ██   ██  ██████  ██   ██        ███████ ██   ██ ██ ██      ██           ",
         "    ██   ██ ██  ████ ██   ██ ██     ██      ██  ██  ██ ██      ██           ",
         "    ███████ ██ ██ ██ ███████        ███████ █████   ██ ██      ██           ",
